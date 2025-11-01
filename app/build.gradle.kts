d13a8e40f079d151313d25b8a0102dac022b22bd
PathForest <k1031oct2000@gmail.com>
2025-11-01 23:46:19 +0900

    setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,16 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
-    namespace = "__PACKAGE_NAME__"
+    namespace = "com.gws.auto.for.android"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "__PACKAGE_NAME__"
+        applicationId = "com.gws.auto.for.android"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -50,6 +52,10 @@ android {
 }
 
 dependencies {
+
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
