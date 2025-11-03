43836f730fc3df9501ffa22fe40a6de27a6a9bbf
PathForest <k1031oct2000@gmail.com>
2025-11-03 12:35:08 +0900

    Fix build error by downgrading googleid to 1.0.0 and centralizing repositories

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.4.0")
     implementation("androidx.credentials:credentials:1.5.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.5.0")
-    implementation("com.google.android.libraries.identity:googleid:1.1.0")
+    implementation("com.google.android.libraries.identity:googleid:1.0.0")
     implementation("com.google.api-client:google-api-client-android:2.8.1")
     implementation("com.google.http-client:google-http-client-gson:2.0.2")
     implementation("com.google.apis:google-api-services-drive:v3-rev20230822-2.0.0")
