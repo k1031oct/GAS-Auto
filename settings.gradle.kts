d3bbe65ae8291d0b2356425b5c38bd9725216139
PathForest <k1031oct2000@gmail.com>
2025-11-03 23:15:35 +0900

    fix: Correct Google ID dependency and add JitPack repository
    This commit corrects the group ID for the `googleid` library in `libs.versions.toml` to `com.google.android.libraries.identity.googleid`.
    
    Additionally, it adds the JitPack repository to `settings.gradle.kts` to resolve dependencies hosted there.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { setUrl("https://jitpack.io") }
     }
 }
 rootProject.name = "GWS-Auto-for-Android"
