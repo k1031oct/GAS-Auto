c30d144fa386cc74c56b45c51f60b9624835f7a2
PathForest <k1031oct2000@gmail.com>
2025-11-08 14:07:50 +0900

    refactor: Update kotlin-kapt plugin declaration in `app/build.gradle.kts`
    The method for applying the `kotlin-kapt` plugin has been changed.
    
    Previously, it was applied using a version catalog alias (`libs.plugins.kotlin.kapt`). This has been updated to use the standard `id("org.jetbrains.kotlin.kapt")` syntax for consistency.

--- app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.gws.auto.mobile.android";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 170;
-  public static final String VERSION_NAME = "1.2.170";
+  public static final int VERSION_CODE = 171;
+  public static final String VERSION_NAME = "1.2.171";
 }
