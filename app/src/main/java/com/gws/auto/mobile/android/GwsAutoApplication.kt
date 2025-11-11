6e5ff6fdcccdd5dfaa4cbd3258dc1edbac1c5958
PathForest <k1031oct2000@gmail.com>
2025-11-11 20:28:34 +0900

    refactor: Remove duplicate GwsAutoApplication file
    The file `GwsAutoApplication.kt` was a duplicate and has been deleted.

--- app/src/main/java/com/gws/auto/mobile/android/GwsAutoApplication.kt
+++ app/src/main/java/com/gws/auto/mobile/android/GwsAutoApplication.kt
@@ -1,18 +1,2 @@
+// This file is a duplicate and is no longer needed.
 package com.gws.auto.mobile.android
-
-import android.app.Application
-import com.gws.auto.mobile.android.BuildConfig
-import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
-
-@HiltAndroidApp
-class GwsAutoApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-            Timber.plant(FileLoggingTree(this))
-        }
-    }
-}
